756842d595fb21baca32f820849653b4b39793b8
mateusdeitos <mate.deitos@hotmail.com>
2022-02-12 16:20:42 -0300

    Tree com componente customizado

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ITreeNodeProps, TreeNode } from './TreeView/TreeNode';
 import { useState } from 'react';
 import { TreeSemFetch } from './TreeSemFetch';
 import { TreeComFetch } from './TreeComFetch';
+import { TreeCustomComponent } from './TreeCustomComponent';
 
 function App() {
   return (
@@ -15,30 +16,12 @@ function App() {
       <h3>Tree com fetching de filhos</h3>
       <TreeComFetch />
       <hr />
+      <div className="spacer"></div>
+      <h3>Tree com componente customizado</h3>
+      <TreeCustomComponent />
+      <hr />
     </div>
   );
 }
-
-const CustomTreeNodeComponent: React.FC<ITreeNodeProps> = ({ ...props }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const { level, node } = props;
-  const toggle = () => setIsOpen(!isOpen);
-  return (
-    <TreeNode.Container {...props}>
-      <TreeNode.ToggleButton toggle={toggle} {...props}>
-        {isOpen ? 'close' : 'open'}
-      </TreeNode.ToggleButton>
-      {isOpen && (
-        <div>
-          <p>Level: {level}</p>
-          <p>Id: {node.nodeId}</p>
-          <p>descrição: {node.description}</p>
-          <p>parentNodeID: {node.parentNode?.nodeId || 0}</p>
-          <p>Filhos: {node.childrenNodes.length}</p>
-        </div>
-      )}
-    </TreeNode.Container>
-  );
-};
 
 export default App;
---
+++ src/TreeCustomComponent.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import { getCategories } from './data';
+import { ITreeNode, TreeView } from './TreeView';
+import { ITreeNodeProps, TreeNode } from './TreeView/TreeNode';
+
+const categorias = getCategories();
+export const TreeCustomComponent = () => {
+  const [selected, setSelected] = useState<ITreeNode[]>([]);
+  return (
+    <TreeView
+      nodes={categorias}
+      onSelect={(nodes) => setSelected(nodes)}
+      allowMultiSelect
+      isSelectable={(categoria, level) => true}
+      CustomTreeNodeComponent={CustomTreeNodeComponent}
+    />
+  );
+};
+
+const CustomTreeNodeComponent: React.FC<ITreeNodeProps> = ({ ...props }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { level, node } = props;
+  const toggle = () => setIsOpen(!isOpen);
+  return (
+    <>
+      <TreeNode.Container {...props}>
+        <TreeNode.ToggleButton
+          isVisible={node.childrenNodes.length > 0}
+          toggle={toggle}
+          {...props}
+        >
+          {isOpen ? 'close' : 'open'}
+        </TreeNode.ToggleButton>
+        {node.description} ({node.nodeId})
+        {isOpen && (
+          <div>
+            <p>Level: {level}</p>
+            <p>Id: {node.nodeId}</p>
+            <p>descrição: {node.description}</p>
+            <p>parentNodeID: {node.parentNode?.nodeId || 0}</p>
+            <p>Filhos: {node.childrenNodes.length}</p>
+          </div>
+        )}
+      </TreeNode.Container>
+      {isOpen &&
+        node.childrenNodes.map((child) => (
+          <CustomTreeNodeComponent
+            key={node.nodeId}
+            node={child}
+            level={level + 1}
+          />
+        ))}
+    </>
+  );
+};
